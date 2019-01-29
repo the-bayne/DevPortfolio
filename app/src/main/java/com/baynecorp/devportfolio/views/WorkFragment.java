c82a29c1f03be9cd7b07ea020136aef9d5e71c5d
Jared Thibodeaux <admin@baynecorp.com>
2019-01-28 20:57:20 -0600

    Added MVC folder structure Created Fragments for the TabLayout

---
+++ app/src/main/java/com/baynecorp/devportfolio/views/WorkFragment.java
@@ -0,0 +1,31 @@
+package com.baynecorp.devportfolio.views;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.baynecorp.devportfolio.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkFragment extends Fragment {
+
+
+    public WorkFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View workView = inflater.inflate(R.layout.fragment_work, container, false);
+        // Inflate the layout for this fragment
+        return workView;
+    }
+
+}
